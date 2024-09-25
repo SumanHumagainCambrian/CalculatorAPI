22ad3e096f37ff3e97a86526e4c40499e330778b
Suman Humagain <returnzerosh@gmail.com>
2024-09-25 15:52:07 -0400

    Addded additiona and subtraction methods

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -7,6 +7,22 @@ namespace CalculatorAPI.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        // GET add action
+        [HttpGet("add")]
+        public ActionResult<decimal> Add(decimal param1, decimal param2)
+        {
+            var result = param1 + param2;
+            return Ok(result);
+        }
+
+        // GET subtract action
+        [HttpGet("subtract")]
+        public ActionResult<decimal> Subtract(decimal param1, decimal param2)
+        {
+            var result = param1 - param2;
+            return Ok(result);
+        }
+
 
     }
 }
